353927179bef9dffb86d45752b3ff3c99554e3c2
Llandy Riveron Del Risco <llandy3d@gmail.com>
2020-05-04 00:03:29 +0200

    Update link_dotfiles.sh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,3 +2,4 @@ mkdir -p $HOME/.vim && ln -s $(pwd)/coc-settings.json $HOME/.vim/
 ln -s $(pwd)/.vimrc $HOME/
 ln -s $(pwd)/.inputrc $HOME/
 ln -s $(pwd)/.pythonrc.py $HOME/
+ln -s $(pwd)/flake8 $HOME/.config/
