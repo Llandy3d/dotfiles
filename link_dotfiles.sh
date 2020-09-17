8a27d8eeddc0797f4a4a1348a5cdd0a0c8df1bf0
Llandy Riveron Del Risco <llandy3d@gmail.com>
2020-09-18 01:05:33 +0200

    firefox environment variables for wayland

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -7,6 +7,7 @@ ln -s $(pwd)/.inputrc $HOME/
 ln -s $(pwd)/.pythonrc.py $HOME/
 ln -s $(pwd)/flake8 $HOME/.config/
 ln -s $(pwd)/.gitconfig $HOME/
+ln -s $(pwd)/.pam_environment $HOME/
 rm $HOME/.bashrc && ln -s $(pwd)/.bashrc $HOME/
 
 for file in $(pwd)/bin/* ; do ln -s $file $HOME/.local/bin/ ; done
