d9b4d8cb3767eeee9540b30f82a4fda5a26900c0
Llandy Riveron Del Risco <llandy3d@gmail.com>
2020-09-18 23:07:58 +0200

    initial waybar config

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,5 +1,4 @@
 mkdir -p $HOME/.vim && ln -s $(pwd)/coc-settings.json $HOME/.vim/
-mkdir -p $HOME/.config/sway && ln -s $(pwd)/.config/sway/config $HOME/.config/sway/
 mkdir -p $HOME/.config/alacritty && ln -s $(pwd)/.config/alacritty/alacritty.yml $HOME/.config/alacritty/
 ln -s $(pwd)/.vimrc $HOME/
 ln -s $(pwd)/after $HOME/.vim/
@@ -8,6 +7,8 @@ ln -s $(pwd)/.pythonrc.py $HOME/
 ln -s $(pwd)/flake8 $HOME/.config/
 ln -s $(pwd)/.gitconfig $HOME/
 ln -s $(pwd)/.pam_environment $HOME/
+ln -s $(pwd)/.config/sway $HOME/.config
+ln -s $(pwd)/.config/waybar $HOME/.config/
 rm $HOME/.bashrc && ln -s $(pwd)/.bashrc $HOME/
 
 for file in $(pwd)/bin/* ; do ln -s $file $HOME/.local/bin/ ; done
